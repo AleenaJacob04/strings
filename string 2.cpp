d57ff1977942d8ec57bf22ef03023a24134bf3a1
AleenaJacob04 <141300254+AleenaJacob04@users.noreply.github.com>
2023-08-10 10:25:13 +0530

    Add files via upload

---
+++ string 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+    char str2[8]={'s','i','t','e','&','t','c','\0'};
+    char str1[20];
+    cout<<"enter the string";
+    cin.get(str1,20);
+    cout<<str1;
+
+         
+    
+    return 0;
+}
---
+++ string1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char str1[]="SITE&TC";
+    char str2[8]={'s','i','t','e','&','t','c','\0'};
+     cout<<str1<<endl;
+     cout<<str2<<endl;
+     return 0;
+}
